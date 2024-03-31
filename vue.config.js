ae03b1a0a3bdcfec870de78e62e68906ba1392c9
Juliana <j.storka@web.de>
2024-03-31 16:14:20 +0200

    modified config

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-// vue.config.js file to be placed in the root of your repository
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/iu-ipwa01-01-co2-footprint/'
-    : '/'
-}
+
 
